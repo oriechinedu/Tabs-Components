99a49ce336f43b012eef172ba2298beca3d7c76d
Orie Chinedu <oriechinedu@gmail.com>
2019-05-16 13:50:16 +0100

    Implement tab component

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -25,6 +25,4 @@ class Dropdown {
 
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
-// let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
-const dropdown = document.querySelector('.dropdown')
-new Dropdown(dropdown)
+let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
